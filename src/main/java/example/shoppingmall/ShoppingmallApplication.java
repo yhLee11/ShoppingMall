efc46c4968de3eeb02d0dd93d319c16f171935b7
yhLee11 <mrncd1@gmail.com>
2022-08-18 12:20:25 +0900

    initial setting

--- src/main/java/example/shoppingmall/ShoppingmallApplication.java
+++ src/main/java/example/shoppingmall/ShoppingmallApplication.java
@@ -1,4 +1,4 @@
-package com.example.shoppingmall;
+package example.shoppingmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/example/shoppingmall/ShoppingmallApplicationTests.java
+++ src/test/java/example/shoppingmall/ShoppingmallApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.shoppingmall;
+package example.shoppingmall;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
