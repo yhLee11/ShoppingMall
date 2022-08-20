983e76ac8baefc625e58e6d17bdf92a64959db05
yhLee11 <mrncd1@gmail.com>
2022-08-20 13:44:55 +0900

    회원가입 컨트롤러

---
+++ src/main/java/example/shoppingmall/controller/MemberController.java
@@ -0,0 +1,43 @@
+package example.shoppingmall.controller;
+
+import example.shoppingmall.dto.MemberFormDto;
+import example.shoppingmall.enitity.Member;
+import example.shoppingmall.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@RequestMapping("/members")
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/new")
+    public String memberForm(Model model) {
+        model.addAttribute("memberFormDto", new MemberFormDto());
+        return "member/memberForm";
+    }
+
+    @PostMapping("/new")
+    public String memberForm(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "member/memberForm";
+        }
+        try {
+            Member member = Member.createMember(memberFormDto);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
+
+        return "redirect:/";
+    }
+}
